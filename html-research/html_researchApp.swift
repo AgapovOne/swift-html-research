065eaad497086c5fd6a707b436ebf0d4edf9b642
Alexey Agapov <agapov.one@gmail.com>
2022-08-09 16:26:22 +0500

    Initial Commit

---
+++ html-research/html_researchApp.swift
@@ -0,0 +1,17 @@
+//
+//  html_researchApp.swift
+//  html-research
+//
+//  Created by Alexey Agapov on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct html_researchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
